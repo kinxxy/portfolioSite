5672fbd1194b233b4722a723d4612b2906477996
kinxxy <hastingskinggold@gmail.com>
2024-10-02 06:43:07 -0400

    Update index.jsx
    updated Bio

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -40,7 +40,7 @@ const About = () => {
           <h2 className="primary__title">About Me</h2>
           <h1 className="title">I am <span className="color__primary">Hastings Gold</span></h1>
           <p className="text__muted description">
-          A Full Stack Web Developer skilled in both front-end and back-end technologies, 
+          I am a Full Stack Web Developer skilled in both front-end and back-end technologies, 
           capable of building dynamic and responsive web applications. Proficient in HTML, 
           CSS, JavaScript, and frameworks like React or Angular on the front end, as well 
           as Node.js, Python, or PHP on the back end. Experienced in database management, 
